968ba6ca8a92aea5b765331b97a2bfdc9846c11c
Daniel Gomez <daniel@esdanielgomez.com>
2022-05-07 15:36:46 -0500

    Add project files.

---
+++ App.PL/ViewModels/DefaultViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.BL.Models;
+using App.BL.Services;
+using DotVVM.Framework.ViewModel;
+
+namespace App.PL.ViewModels
+{
+    public class DefaultViewModel : MasterPageViewModel
+    {
+        private readonly StudentService studentService;
+
+        public DefaultViewModel(StudentService studentService)
+        {
+            this.studentService = studentService;
+        }
+
+        [Bind(Direction.ServerToClient)]
+        public List<StudentListModel> Students { get; set; }
+
+        public override async Task PreRender()
+        {
+            Students = await studentService.GetAllStudentsAsync();
+            await base.PreRender();
+        }
+    }
+}
